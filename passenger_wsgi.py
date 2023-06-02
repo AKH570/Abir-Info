0d4c414c72c7b8351e838305cae510edf1aa19d3
AKH570 <khandokar.13@gmail.com>
2023-06-03 02:34:26 +0600

    passenger file delete

--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-#this is for local passenger
-import os
-import sys
-from Abir.wsgi import application
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-environ = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Abir.settings")
